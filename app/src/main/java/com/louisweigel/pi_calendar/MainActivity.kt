5a2f0ccd742e9156bdeac37cbf03d63693ff9219
TheBlckbird <theblckbrd@proton.me>
2026-02-26 21:32:24 +0100

    Rework FAB menu (#4)
    * Split up Fab Menu for adding events[5~
    
    * Add click-off for FAB menu

--- app/src/main/java/com/louisweigel/pi_calendar/MainActivity.kt
+++ app/src/main/java/com/louisweigel/pi_calendar/MainActivity.kt
@@ -4,77 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
+import com.louisweigel.pi_calendar.screens.AddEventsMenu
 import com.louisweigel.pi_calendar.screens.CounterView
+import com.louisweigel.pi_calendar.screens.TopBar
 import com.louisweigel.pi_calendar.ui.theme.PicalendarTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-
-class AddEventButtonViewModel : ViewModel() {
-    private val _isAddEventMenuOpen = MutableStateFlow(false)
-    val isAddEventMenuOpen: StateFlow<Boolean> = _isAddEventMenuOpen.asStateFlow()
-
-    fun toggleMenu() {
-        _isAddEventMenuOpen.value = !_isAddEventMenuOpen.value
-    }
-
-    fun closeMenu() {
-        _isAddEventMenuOpen.value = false
-    }
-}
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -82,189 +46,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var isExpanded by remember { mutableStateOf(false) }
+
+            val boxAlphaValue = if (isExpanded) 0.7f else 0.0f
+            val boxAlpha = animateFloatAsState(targetValue = boxAlphaValue)
+
             PicalendarTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        TopAppBar(
-                            navigationIcon = {
-                                IconButton({}) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Menu,
-                                        contentDescription = "Menu"
-                                    )
-                                }
-                            },
-
-                            title = {
-                                TextButton(
-                                    {},
-                                    shape = RoundedCornerShape(8.dp),
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .padding(vertical = 8.dp),
-                                    colors = ButtonDefaults.textButtonColors().copy(
-                                        contentColor = MaterialTheme.colorScheme.onSurface
-                                    )
-                                ) {
-                                    Text(
-                                        "Februar",
-                                        style = MaterialTheme.typography.titleLarge
-                                    )
-                                    Icon(
-                                        imageVector = Icons.Default.ArrowDropDown,
-                                        contentDescription = "Dropdown öffnen"
-                                    )
-                                }
-                            },
-
-                            actions = {
-                                IconButton({}) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Search,
-                                        contentDescription = "Suche",
-                                    )
-                                }
-
-                                IconButton({}) {
-                                    Icon(
-                                        imageVector = Icons.Filled.DateRange,
-                                        contentDescription = "Heute fokussieren"
-                                    )
-                                }
-                            }
+                    topBar = { TopBar() },
+                    floatingActionButton = {
+                        AddEventsMenu(
+                            isExpanded,
+                            { isExpanded = !isExpanded },
+                            { isExpanded = false },
                         )
                     },
-
-                    floatingActionButton = {
-                        var isExpanded by remember { mutableStateOf(false) }
-                        val fabRadius = if (isExpanded) {
-                            28.dp
-                        } else {
-                            16.dp // makes it a circle, its size is 56dp
-                        }
-                        val fabCornerRadius = animateDpAsState(targetValue = fabRadius)
-
-                        Column(
-                            horizontalAlignment = Alignment.End,
-                            verticalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-
-                            Column(
-                                horizontalAlignment = Alignment.End,
-                                verticalArrangement = Arrangement.spacedBy(4.dp)
-                            ) {
-                                AnimatedVisibility(
-                                    visible = isExpanded,
-                                    enter = slideInVertically(initialOffsetY = { it * 1 }) + fadeIn(),
-                                    exit = slideOutVertically(targetOffsetY = { it * 1 }) + fadeOut()
-                                ) {
-                                    Button(
-                                        onClick = {},
-                                        colors = ButtonDefaults.buttonColors(
-                                            contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                                            containerColor = MaterialTheme.colorScheme.primaryContainer
-                                        ),
-                                        modifier = Modifier.height(56.dp)
-                                    ) {
-                                        Row(
-                                            horizontalArrangement = Arrangement.spacedBy(8.dp),
-                                            verticalAlignment = Alignment.CenterVertically
-                                        ) {
-                                            Icon(
-                                                imageVector = Icons.Default.DateRange,
-                                                contentDescription = null
-                                            )
-                                            Text(
-                                                "Geburtstag",
-                                                style = MaterialTheme.typography.titleMedium
-                                            )
-                                        }
-                                    }
-                                }
-
-                                AnimatedVisibility(
-                                    visible = isExpanded,
-                                    enter = slideInVertically(initialOffsetY = { it * 2 }) + fadeIn(),
-                                    exit = slideOutVertically(targetOffsetY = { it * 2 }) + fadeOut()
-                                ) {
-                                    Button(
-                                        onClick = {},
-                                        colors = ButtonDefaults.buttonColors(
-                                            contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                                            containerColor = MaterialTheme.colorScheme.primaryContainer
-                                        ),
-                                        modifier = Modifier.height(56.dp)
-                                    ) {
-                                        Row(
-                                            horizontalArrangement = Arrangement.spacedBy(8.dp),
-                                            verticalAlignment = Alignment.CenterVertically
-                                        ) {
-                                            Icon(
-                                                imageVector = Icons.Default.DateRange,
-                                                contentDescription = null
-                                            )
-                                            Text(
-                                                "Erinnerung",
-                                                style = MaterialTheme.typography.titleMedium
-                                            )
-                                        }
-                                    }
-                                }
-
-                                AnimatedVisibility(
-                                    visible = isExpanded,
-                                    enter = slideInVertically(initialOffsetY = { it * 3 }) + fadeIn(),
-                                    exit = slideOutVertically(targetOffsetY = { it * 3 }) + fadeOut()
-                                ) {
-                                    Button(
-                                        onClick = {},
-                                        colors = ButtonDefaults.buttonColors(
-                                            contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                                            containerColor = MaterialTheme.colorScheme.primaryContainer
-                                        ),
-                                        modifier = Modifier.height(56.dp)
-                                    ) {
-                                        Row(
-                                            horizontalArrangement = Arrangement.spacedBy(8.dp),
-                                            verticalAlignment = Alignment.CenterVertically
-                                        ) {
-                                            Icon(
-                                                imageVector = Icons.Default.DateRange,
-                                                contentDescription = null
-                                            )
-                                            Text(
-                                                "Termin",
-                                                style = MaterialTheme.typography.titleMedium
-                                            )
-                                        }
-                                    }
-                                }
-                            }
-
-                            FloatingActionButton(
-                                onClick = { isExpanded = !isExpanded },
-                                shape = RoundedCornerShape(fabCornerRadius.value),
-                                contentColor = if (isExpanded) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onPrimaryContainer,
-                                containerColor = if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.primaryContainer
-                            ) {
-                                Row(
-                                    modifier = Modifier.padding(horizontal = 8.dp),
-                                    verticalAlignment = Alignment.CenterVertically,
-                                    horizontalArrangement = Arrangement.SpaceBetween
-                                ) {
-                                    Icon(
-                                        imageVector = if (isExpanded) Icons.Default.Close else Icons.Default.Add,
-                                        contentDescription = "Write or Reflect Today's Reflection",
-                                        modifier = if (isExpanded) Modifier.size(20.dp) else Modifier // Makes things look more natural
-                                    )
-                                }
-                            }
-                        }
-                    }
-
                 ) { innerPadding ->
-                    MainContent(Modifier.padding(innerPadding))
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .pointerInput(Unit) {
+                                detectTapGestures(onTap = { isExpanded = false })
+                            }
+                    ) {
+                        MainContent()
+                    }
                 }
             }
         }
@@ -272,8 +80,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainContent(modifier: Modifier) {
-    Column(modifier) {
+fun MainContent() {
+    Column {
         CounterView()
 
         LazyColumn(Modifier.fillMaxSize()) {
@@ -282,65 +90,6 @@ fun MainContent(modifier: Modifier) {
                     text = "Number ${index + 1}",
                     modifier = Modifier.padding(8.dp)
                 )
-            }
-        }
-    }
-}
-
-@Composable
-fun AddEventButton(isMenuOpen: Boolean, onClick: () -> Unit) {
-    FloatingActionButton(onClick) {
-        Icon(
-            if (isMenuOpen) Icons.Default.Close else Icons.Default.Add,
-            "Neues Event erstellen"
-        )
-    }
-}
-
-@Composable
-fun FabMenuItems(
-    isMenuOpen: Boolean,
-    menuItems: List<Pair<String, () -> Unit>>,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = modifier.padding(bottom = 72.dp)
-    ) {
-        menuItems.forEachIndexed { index, item ->
-            AnimatedVisibility(
-                visible = isMenuOpen,
-                enter = slideInVertically(initialOffsetY = { it * (index + 1) }) + fadeIn(),
-                exit = slideOutVertically(targetOffsetY = { it * (index + 1) }) + fadeOut()
-            ) {
-                FloatingActionButton(onClick = item.second) {
-                    Text(item.first, style = MaterialTheme.typography.labelLarge)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun FabMenuScreen(viewModel: AddEventButtonViewModel = viewModel()) {
-    val isMenuOpen by viewModel.isAddEventMenuOpen.collectAsState()
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        FabMenuItems(
-            isMenuOpen = isMenuOpen,
-            menuItems = listOf(
-                "Edit" to { viewModel.closeMenu(); /* Handle edit */ },
-                "Share" to { viewModel.closeMenu(); /* Handle share */ }
-            )
-        )
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            contentAlignment = Alignment.BottomEnd
-        ) {
-            AddEventButton(isMenuOpen = isMenuOpen) {
-                viewModel.toggleMenu()
             }
         }
     }
---
+++ app/src/main/java/com/louisweigel/pi_calendar/screens/AddEventsMenu.kt
@@ -0,0 +1,130 @@
+package com.louisweigel.pi_calendar.screens
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AddEventsMenu(
+    isExpanded: Boolean,
+    onToggleExpanded: () -> Unit,
+    onClose: () -> Unit,
+) {
+    val fabRadius = if (isExpanded) {
+        28.dp
+    } else {
+        16.dp // makes it a circle, its size is 56dp
+    }
+    val fabCornerRadius = animateDpAsState(targetValue = fabRadius)
+
+    Column(
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+
+        Column(
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            FabMenuEntry(0, isExpanded, "Geburtstag", { onClose() }) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = null
+                )
+            }
+
+            FabMenuEntry(1, isExpanded, "Erinnerung", { onClose() }) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = null
+                )
+            }
+
+            FabMenuEntry(2, isExpanded, "Termin", { onClose() }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = null
+                )
+            }
+        }
+
+        FloatingActionButton(
+            onClick = onToggleExpanded,
+            shape = RoundedCornerShape(fabCornerRadius.value),
+            contentColor = if (isExpanded) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onPrimaryContainer,
+            containerColor = if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.primaryContainer
+        ) {
+            Row(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Icon(
+                    imageVector = if (isExpanded) Icons.Default.Close else Icons.Default.Add,
+                    contentDescription = "Write or Reflect Today's Reflection",
+                    modifier = if (isExpanded) Modifier.size(20.dp) else Modifier // Makes things look more natural
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun FabMenuEntry(
+    index: Int,
+    isExpanded: Boolean,
+    title: String,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit
+) {
+    AnimatedVisibility(
+        visible = isExpanded,
+        enter = slideInVertically(initialOffsetY = { it * (index + 1) }) + fadeIn(),
+        exit = slideOutVertically(targetOffsetY = { it * (index + 1) }) + fadeOut()
+    ) {
+        Button(
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            ),
+            modifier = Modifier.height(56.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                icon()
+                Text(
+                    title,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
+    }
+}
