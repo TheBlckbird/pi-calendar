5b82b77cfc2cae7becc3d0e7f35a64b812659119
TheBlckbird <theblckbrd@proton.me>
2026-05-11 07:28:35 +0200

    Move `observeAll` for calendar entries to repository (#75)

--- app/src/main/java/com/louisweigel/pi_calendar/core/db/repositories/CalendarEntryRepository.kt
+++ app/src/main/java/com/louisweigel/pi_calendar/core/db/repositories/CalendarEntryRepository.kt
@@ -7,6 +7,7 @@ import com.louisweigel.pi_calendar.core.calendarentry.Event
 import com.louisweigel.pi_calendar.core.calendarentry.Reminder
 import com.louisweigel.pi_calendar.core.db.daos.CalendarEntryDao
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import java.util.UUID
 
 class CalendarEntryRepository(
@@ -15,11 +16,29 @@ class CalendarEntryRepository(
     suspend fun getAll(): List<CalendarEntry> =
         dao.getAll()
 
-    fun observeAll(): Flow<List<CalendarEntry>> =
-        dao.observeAll()
+    fun observeAll(): Flow<List<CalendarEntry>> = combine(
+        dao.observeEvents(), dao.observeBirthdays(), dao.observeReminders()
+    ) { events, birthdays, reminders ->
+        (events + birthdays + reminders).sortedBy { it.date }
+    }
 
-    fun observeAllWithCalendar(): Flow<List<Pair<Calendar, CalendarEntry>>> =
-        dao.observeAllWithCalendar()
+    /**
+     * Gets all calendar entries with their respective calendar as an observable
+     *
+     * Doesn't include entries from hidden calendars
+     */
+    fun observeAllWithCalendar(): Flow<List<Pair<Calendar, CalendarEntry>>>
+         = combine(
+            dao.observeAllEventsWithCalendar(),
+            dao.observeAllBirthdaysWithCalendar(),
+            dao.observeAllRemindersWithCalendar()
+        ) { events, birthdays, reminders ->
+            (
+                    events.map { Pair(it.calendar, it.event) }
+                            + birthdays.map { Pair(it.calendar, it.birthday) }
+                            + reminders.map { Pair(it.calendar, it.reminder) }
+                    )
+        }
 
     suspend fun insert(entry: CalendarEntry) {
         when (entry) {
